b122f533a7efb657ccfe5a5eaf43207957213ec6
pongsatorn bhothikaew < GitHub>
2026-07-01 12:11:30 +0700

    chore: bump service worker cache to force all clients to redownload fresh uncorrupted html

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿// HOME PPK 2026 � Service Worker v1
-var CACHE_NAME = 'ppk-v20260527C';
+// HOME PPK 2026 � Service Worker v1
+var CACHE_NAME = 'ppk-v20260701A';
 var PRECACHE = [
   './',
   './dashboard.html',
